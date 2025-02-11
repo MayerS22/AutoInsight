0e5f155224c50702c646e606bc10e4e332d62737
mazen <mazenraafat568@gmail.com>
2025-02-11 23:57:19 +0200

    Merging

--- AutoInsight/src/util/dashboard.js
+++ AutoInsight/src/util/dashboard.js
@@ -1,5 +1,5 @@
 export const dashboards = [
-    { id: 1, name: "Marketing Analytics", datasetName: "cancer", time: "Feb 1, 2024",usersWithPermission:["mazen","maya","farah","menna"] },
+    { id: 1, name: "Marketing Analytics", datasetName: "cancer", time: "Feb 1, 2024",usersWithPermission:["mayer","maya","farah","menna"] },
     { id: 2, name: "Sales Report Q1", datasetName: "sales", time: "Jan 15, 2024" ,usersWithPermission:["mazen","bishoy"]},
     { id: 3, name: "Customer Insights", datasetName: "enginner", time: "Dec 20, 2023" ,usersWithPermission:["ahmed alaa","3bshafy","osama"]},
     { id: 4, name: "Website Traffic", datasetName: "asdasdk", time: "Nov 5, 2023" ,usersWithPermission:["peter"]},

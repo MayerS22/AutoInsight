ac897290bc4507ec91c8fa0043b1fac9d624c81d
mayers22 <mayersoliman4@gmail.com>
2025-04-29 00:46:15 +0300

    admin

--- AutoInsight/Frontend/src/components/Admin/AdminDashboard.jsx
+++ AutoInsight/Frontend/src/components/Admin/AdminDashboard.jsx
@@ -213,24 +213,6 @@ useEffect(() => {
   fetchDashboardData();
 }, []);
 
-
-  if(!isAdmin){
-    return (
-      <div className="flex items-center justify-center h-screen">
-      <div className="bg-white p-8 rounded-lg shadow-md text-center">
-          <h2 className="text-2xl font-bold text-purple-800">Access Denied</h2>
-          <p className="text-gray-600 mt-2">You are not an admin</p>
-          <p 
-            
-              className="mt-4 px-4 py-2 text-purple-700 rounded-lg font-bold"
-          >
-              Go to Login
-          </p>
-      </div>
-     </div>
-    )
-  }
-
   return (
     <div className="bg-purple-50 min-h-screen pt-20">
       <div className="max-w-8xl mx-auto p-8">
